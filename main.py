4b540ab7d1a780465e954d20f2600ce40c2401c5
chang shuan <changshuan@changs-MacBook-Air.local>
2025-10-29 22:16:10 +0800

    update code

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import requests
 import json
 
 #store a api
-url = "https://jsonplaceholder.typicode.com/users/1"
+url = "https://uselessfacts.jsph.pl/random.json?language=en"
 
 #get a request
 response= requests.get(url)
 
-print(response.json())
+if response.status_code==200:
+    data = response.json()
+    print("Here is the random fact:", data["text"])
+else:
+    print(f"Error: {response.status_code}")
