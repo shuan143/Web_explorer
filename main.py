31060734742ec6f3186cb9e6276be4b2d592d156
chang shuan <changshuan@changs-MacBook-Air.local>
2025-10-29 22:57:55 +0800

    define a function which can write new fact in file

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import requests
 import json
+import os
 
-#store a api
-url = "https://uselessfacts.jsph.pl/random.json?language=en"
+#store a url
+URL = "https://uselessfacts.jsph.pl/random.json?language=en"
 
-#get a request
-response= requests.get(url)
-
-if response.status_code==200:
-    data = response.json()
-    print("Here is the random fact:", data["text"])
-else:
-    print(f"Error: {response.status_code}")
+def catch(url):
+    #get fact
+    response = requests.get(url)
+    if response.status_code == 200:
+        #load the fact_list or create a empty fact_list
+        if(os.path.exists("fact.json")):
+            with open("fact.json", "r") as file:
+                fact=json.load(file)
+        else:
+            fact={"fact_list":[]}
+        data =response.json()
+        #check if new fact has exist
+        have_exist= data["text"] in fact["fact_list"]
+        #if new has not exist in fact_list, then add in it
+        if  not have_exist:
+            with open("fact.json", "w") as file:
+                fact["fact_list"].append(data["text"])
+                json.dump(fact, file)
+            print("Successfully add a fact to fact_list!")
+            print("Include:", data["text"])
+    else:
+        print(f"Error: {response.status_code}")
+for x in range(3):
+    catch(URL)
